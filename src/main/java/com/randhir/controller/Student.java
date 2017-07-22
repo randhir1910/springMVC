7f667bb6a8cc41f341d0bb5d3f33a95df827319e
randhir <randhir.kumar@tothenew.com>
2017-07-23 05:06:36 +0530

    ques3

--- src/main/java/com/randhir/controller/Student.java
+++ src/main/java/com/randhir/controller/Student.java
@@ -1,5 +1,8 @@
 package com.randhir.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
 import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
@@ -7,16 +10,13 @@ import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class Student extends MultiActionController
+@Controller
+public class Student
 {
-
-    protected ModelAndView handleRequestInternal(javax.servlet.http.HttpServletRequest httpServletRequest, javax.servlet.http.HttpServletResponse httpServletResponse) throws Exception {
-       ModelAndView view=new ModelAndView("index","msg","index.jsp");
-       return  view;
-    }
-    public  void methodShow(HttpServletRequest request, HttpServletResponse response) throws  Exception
-    {
-        response.setContentType("text/html");
-       response.getWriter().println("<b>HELLO World</b>");
-    }
+     @RequestMapping("/")
+     @ResponseBody
+   String index()
+   {
+       return "Hello World";
+   }
 }
