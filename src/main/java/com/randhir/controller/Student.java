42178bb7ff91c5a9924b58af2bc266f9fbcb213f
randhir <randhir.kumar@tothenew.com>
2017-07-23 00:49:48 +0530

    ques2

--- src/main/java/com/randhir/controller/Student.java
+++ src/main/java/com/randhir/controller/Student.java
@@ -2,12 +2,21 @@ package com.randhir.controller;
 
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
+import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
 
-public class Student extends AbstractController
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Student extends MultiActionController
 {
 
     protected ModelAndView handleRequestInternal(javax.servlet.http.HttpServletRequest httpServletRequest, javax.servlet.http.HttpServletResponse httpServletResponse) throws Exception {
        ModelAndView view=new ModelAndView("index","msg","index.jsp");
        return  view;
+    }
+    public  void methodShow(HttpServletRequest request, HttpServletResponse response) throws  Exception
+    {
+        response.setContentType("text/html");
+       response.getWriter().println("<b>HELLO World</b>");
     }
 }
