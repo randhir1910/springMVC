1d3c496b91a6061ab870c93c0c71459f792c81a5
randhir <randhir.kumar@tothenew.com>
2017-07-23 06:23:30 +0530

    ques6

--- src/main/java/com/randhir/controller/Student.java
+++ src/main/java/com/randhir/controller/Student.java
@@ -1,24 +1,21 @@
 package com.randhir.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.AbstractController;
-import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
 
 @Controller
 public class Student
 {
-    @RequestMapping(value ="hello/{firstName}/{lastName}")
-     @ResponseBody
-   String index(@PathVariable("firstName") String firstName,@PathVariable("lastName") String lastName)
-   {
-       return firstName+lastName;
-   }
+    @RequestMapping(value = "/countryAndState/{country}/{state}")
+  public ModelAndView CountryState(@PathVariable Map<String,String>  stringMap)
+    {
+          String country=stringMap.get("country");
+          String state=stringMap.get("state");
+          ModelAndView view =new ModelAndView("index");
+          view.addObject("msg"," country  "+country+" State "+state);
+          return  view;
+    }
 }
