7cbdb950973e86c9cfa80d3bcde4b522bc77ff81
randhir <randhir.kumar@tothenew.com>
2017-07-23 06:35:10 +0530

    Ques7

--- src/main/java/com/randhir/controller/Student.java
+++ src/main/java/com/randhir/controller/Student.java
@@ -3,19 +3,19 @@ package com.randhir.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import java.util.Map;
 
 @Controller
 public class Student
 {
-    @RequestMapping(value = "/countryAndState/{country}/{state}")
-  public ModelAndView CountryState(@PathVariable Map<String,String>  stringMap)
+    @RequestMapping(value = "/hello",method = RequestMethod.POST)
+  public ModelAndView CountryState(@RequestParam("firstName") String fname,@RequestParam("lastName") String lname)
     {
-          String country=stringMap.get("country");
-          String state=stringMap.get("state");
           ModelAndView view =new ModelAndView("index");
-          view.addObject("msg"," country  "+country+" State "+state);
+          view.addObject("msg"," firstname  "+fname+" lastname "+lname);
           return  view;
     }
 }
---
+++ target/spring-mvc-maven/FormData.jsp
@@ -0,0 +1,24 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: randhir
+  Date: 23/7/17
+  Time: 6:25 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<form action="/hello" method="post">
+    <label>First Name</label>
+    <input type="text" name="firstName">
+    <br>
+    <label>Last Name</label>
+    <input type="text" name="lastName">
+    <br>
+    <input type="submit" value="Submit">
+</form>
+</body>
+</html>
