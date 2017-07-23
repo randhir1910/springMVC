a3ff6674e62046e450c9d63ba7e9e81d16811f25
randhir <randhir.kumar@tothenew.com>
2017-07-23 06:56:14 +0530

    Ques8

--- src/main/java/com/randhir/controller/Student.java
+++ src/main/java/com/randhir/controller/Student.java
@@ -1,21 +1,21 @@
 package com.randhir.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import java.util.Map;
-
+import com.randhir.model.studentModel;
 @Controller
 public class Student
 {
     @RequestMapping(value = "/hello",method = RequestMethod.POST)
-  public ModelAndView CountryState(@RequestParam("firstName") String fname,@RequestParam("lastName") String lname)
+  public ModelAndView CountryState(@ModelAttribute studentModel model )
     {
           ModelAndView view =new ModelAndView("index");
-          view.addObject("msg"," firstname  "+fname+" lastname "+lname);
+          view.addObject("msg"," firstname  "+model.getFirstName()+" lastname "+model.getLastName());
           return  view;
     }
 }
